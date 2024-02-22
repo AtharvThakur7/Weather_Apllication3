f56915ce629ee6cc95d9b7165a6f3c191b4174e3
AtharvThakur7 <134005780+AtharvThakur7@users.noreply.github.com>
2024-02-22 21:28:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import logging as log
+log.basicConfig(filename="Weather_App.log" , level=log.INFO , format='%(asctime)s %(levelname)s %(message)s')
 from flask import Flask, request, render_template, url_for
 import requests
 
@@ -6,19 +8,28 @@ app = Flask(__name__, static_folder='static' , template_folder='templates')
 # route to the Homepage
 @app.route('/page')
 def homepage():
-    return render_template("index.html")
+    try :
+        return render_template("index.html")
+    except Exception as e :
+        log.error(e)
 
 @app.route('/weatherapp', methods=['POST', 'GET'])
 def Get_Weather_Data():
-    url = "https://api.openweathermap.org/data/2.5/weather"
-    param = {
-        'q': request.form.get('city'),
-        'units': 'metric',
-        'appid': '582d7c21a2eb650409ce81a274fc8b9c'
-    }
-    result = requests.get(url, params=param)
-    data = result.json()
-    return data
+    try:
+        url = "https://api.openweathermap.org/data/2.5/weather"
+        param = {
+            'q': request.form.get('city'),
+            'units': 'metric',
+            'appid': '582d7c21a2eb650409ce81a274fc8b9c'
+        }
+        result = requests.get(url, params=param)
+        data = result.json()
+        return data
+    except Exception as e :
+        log.error(e)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    try :
+        app.run(debug=True)
+    except Exception as e :
+        log.error(e)    
